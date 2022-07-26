b659a51ea2b5ff12164fbb67328a06a0d2ddfcdd
anil <anilsai029@gmail.com>
2022-07-27 00:35:18 +0530

    logging module created

---
+++ Housing/logger/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from datetime import datetime
+import os
+from re import L
+
+LOG_DIR = "housing_logs"
+CURRENT_TIMEsTAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%H-%S')}"
+LOG_FILE_NAME = f"log_{CURRENT_TIMEsTAMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+logging.basicConfig(filename=LOG_FILE_PATH,
+        filemode="a",
+        format= "[%(asctime)s---%(levelname)s----%(module)s]===>%(message)s)",
+        level = logging.INFO)
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+from Housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route('/', methods= ["POST","GET"])
 def index():
+    logging.info("we are testing logging module")
     return "Hello people"
 
 
