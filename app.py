05d7095ffaffc5f6826fb22f1731508cad99978a
anil <anilsai029@gmail.com>
2022-07-27 12:41:55 +0530

    All components addes

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from Housing.logger import logging
+import sys
+from Housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route('/', methods= ["POST","GET"])
 def index():
-    logging.info("we are testing logging module")
-    return "Hello people"
+    try:
+        raise Exception("we are testing the Exception module")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        print(housing.error_message)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+        return "Hello people"
 
 
 if __name__ == "__main__":
